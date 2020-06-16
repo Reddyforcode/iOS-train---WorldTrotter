ef49f215a0248758febfa0a7643f06414af49a00
Reddy Tintaya <aaabeeelooon@gmail.com>
2020-06-16 17:33:08 -0400

    GradientLayer - silver challenge

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.blue.cgColor, UIColor.green.cgColor]
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
 
