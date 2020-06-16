c2ebb2c32d753db083468df3d8f2966fd554fa6c
Reddy Tintaya <aaabeeelooon@gmail.com>
2020-06-16 12:25:13 -0400

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Reddy Tintaya Conde on 6/16/20.
+//  Copyright © 2020 Reddy Tintaya Conde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
