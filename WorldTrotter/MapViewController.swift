f8875809c6aa41bf6809c626a2d01ad62e1231c5
Reddy Tintaya <aaabeeelooon@gmail.com>
2020-06-20 22:34:51 -0400

    Implement Tab bar. Chapter 5 - Programatic view

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -6,15 +6,39 @@
 //  Copyright © 2020 Reddy Tintaya Conde. All rights reserved.
 //
 
-import Foundation
+import MapKit
 import UIKit
 
 class MapViewController: UIViewController {
-    override func viewDidLoad() {
-        print("MapViewControllerw loaded its view.")
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satelite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex =  0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+//        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+//        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        topConstraint.isActive = true
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        print("MapViewControllerw appeared.")
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
     }
+    
 }
